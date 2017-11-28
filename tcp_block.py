fc2835e99f08b7f332b8200251211c2617ae39a4
JY <popjy0312@naver.com>
2017-11-28 08:26:57 -0800

    success ver1.0

--- tcp_block.py
+++ tcp_block.py
@@ -17,34 +17,43 @@ PSH = 0x08
 ACK = 0x10
 
 def cb(pkt):
-    if TCP in pkt:
+    if Ether in pkt and IP in pkt and TCP in pkt:
         #pkt[TCP].show()
-        rstpkt = pkt[Ether]/pkt[IP]/pkt[TCP]
-        rstpkt[TCP].remove_payload()
-        del rstpkt[IP].chksum
-        del rstpkt[TCP].chksum
-        """
-        if rstpkt[TCP].load:
-            del rstpkt[TCP].load
-            del rstpkt[IP].chksum
-            del rstpkt[TCP].chksum
-        """
-        #rstpkt = pkt.copy()
+        if pkt[TCP].flags & RST:
+            return
+        mypkt = pkt[Ether]/pkt[IP]/pkt[TCP]
+        mypkt[TCP].remove_payload()
+        del mypkt[IP].chksum
+        del mypkt[TCP].chksum
+
         log.debug("original packet seq is %d" % pkt[TCP].seq)
         log.debug("original packet ack is %d" % pkt[TCP].ack)
-        rstpkt[TCP].seq += 1
+        mypkt[TCP].seq += 1
         #pkt[TCP].ack += 1
-        rstpkt[TCP].flags = RST | ACK
-        log.debug(" forward packet seq is %d" % rstpkt[TCP].seq)
-        log.debug(" forward packet ack is %d" % rstpkt[TCP].ack)
-        sendp(rstpkt, iface=InterFace)
+        mypkt[TCP].flags = RST | ACK
+        log.debug(" forward packet seq is %d" % mypkt[TCP].seq)
+        log.debug(" forward packet ack is %d" % mypkt[TCP].ack)
+        sendp(mypkt, iface=InterFace)
 
-        rstpkt[Ether].dst, rstpkt[Ether].src = rstpkt[Ether].src, rstpkt[Ether].dst
-        rstpkt[IP].src, rstpkt[IP].dst = rstpkt[IP].dst, rstpkt[IP].src
-        rstpkt[TCP].seq, rstpkt[TCP].ack = rstpkt[TCP].ack, rstpkt[TCP].seq
-        log.debug("backward packet seq is %d" % rstpkt[TCP].seq)
-        log.debug("backward packet ack is %d" % rstpkt[TCP].ack)
-        sendp(rstpkt, iface=InterFace)
+
+        if "HTTP" in pkt and pkt[TCP].Raw[:4] == 'HTTP':
+            print "HTTP"
+            mypkt[TCP].flags = FIN | ACK
+            mypkt = mypkt / "blocked"
+
+        try:
+            Raw = pkt[TCP].Raw
+        except:
+            Raw = ""
+        payload_len = len(Raw)
+        if payload_len == 0:
+            payload_len += 1
+        mypkt[Ether].dst, mypkt[Ether].src = pkt[Ether].src, pkt[Ether].dst
+        mypkt[IP].src, mypkt[IP].dst = pkt[IP].dst, pkt[IP].src
+        mypkt[TCP].seq, mypkt[TCP].ack = pkt[TCP].ack, pkt[TCP].seq + payload_len
+        log.debug("backward packet seq is %d" % mypkt[TCP].seq)
+        log.debug("backward packet ack is %d" % mypkt[TCP].ack)
+        sendp(mypkt, iface=InterFace)
 
 def main():
     sniff(iface=InterFace, prn=cb)
