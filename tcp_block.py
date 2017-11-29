df271af581547e23e168722403a47d994425a812
popjy0312 <popjy0312@nvaer.com>
2017-11-29 16:35:15 +0900

    afafter feedback

--- tcp_block.py
+++ tcp_block.py
@@ -7,6 +7,10 @@ if len(sys.argv) != 2:
 
 InterFace = sys.argv[1]
 
+MyMac = get_if_hwaddr(InterFace)
+
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+
 FIN = 0x01
 SYN = 0x02
 RST = 0x04
@@ -15,28 +19,30 @@ ACK = 0x10
 
 def cb(pkt):
     if Ether in pkt and IP in pkt and TCP in pkt:
-        if pkt[TCP].flags & RST:
+        if pkt[TCP].flags & RST or pkt[TCP].flags & FIN:
             return
         mypkt = pkt[Ether]/pkt[IP]/pkt[TCP]
         mypkt[TCP].remove_payload()
         del mypkt[IP].chksum
         del mypkt[TCP].chksum
-
-        mypkt[TCP].seq += 1
-        mypkt[TCP].flags = RST | ACK
-        sendp(mypkt, iface=InterFace)
-
-        if "HTTP" in str(pkt) and pkt[TCP].load[:4] == 'HTTP':
-            print "HTTP"
-            mypkt[TCP].flags = FIN | ACK
-            mypkt = mypkt / "blocked"
+	mypkt[Ether].src = MyMac
 
         try:
             payload_len = len(pkt[TCP].Raw)
         except:
             payload_len = 1
+        
+	mypkt[TCP].seq += payload_len
+        mypkt[TCP].flags = RST | ACK
+        sendp(mypkt, iface=InterFace)
 
-        mypkt[Ether].dst, mypkt[Ether].src = pkt[Ether].src, pkt[Ether].dst
+        if "HTTP" in str(pkt) and pkt[TCP].load.split()[0] in HTTP_METHODS:
+            print "HTTP"
+            mypkt[TCP].flags = FIN | ACK
+            mypkt = mypkt / "blocked\r\n"
+
+
+        mypkt[Ether].dst, mypkt[Ether].src = pkt[Ether].src, MyMac
         mypkt[IP].src, mypkt[IP].dst = pkt[IP].dst, pkt[IP].src
         mypkt[TCP].seq, mypkt[TCP].ack = pkt[TCP].ack, pkt[TCP].seq + payload_len
         sendp(mypkt, iface=InterFace)
